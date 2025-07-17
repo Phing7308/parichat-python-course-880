a04d517659c3d43081d95ab2fe19556577698702
Paricht Mangkonkit <parichat.mang@ku.th>
2025-07-17 15:14:42 +0700

    week2s

--- assignments/week02/quiz.py
+++ assignments/week02/quiz.py
@@ -18,7 +18,21 @@ Below 18.5: Underweight
 
 """
 
+weight = float(input"Weight:"))
+heigth = float(input("Height:"))
+bmi = weigth / height ** 2
+print("You BMI :",bmi)
+if bmi < 18.5:
+    print("Underweight")
 
+if bmi >=18.5 and bmi <= 24.9:
+    print("Normal Weight")
+
+if bmi >=25.0 and bmi <=29.9:
+    print("Overweight")
+
+if bmi >=30.0:
+    print("Obese")
 """
 Question 2: Currency Converter (20 points)
 
